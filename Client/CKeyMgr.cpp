f62e97c76eaa8eb107982e38482e249b105c372b
Seung Chan <73764308+olppaemi@users.noreply.github.com>
2023-02-25 11:03:32 +0900

    Key Manager

--- Client/CKeyMgr.cpp
+++ Client/CKeyMgr.cpp
@@ -1,5 +1,44 @@
 #include "pch.h"
 #include "CKeyMgr.h"
+#include "CCore.h"
+
+int g_arrVK[(int)KEY::LAST] =
+{
+	VK_LEFT,
+	VK_RIGHT,
+	VK_UP,	
+	VK_DOWN,
+
+	'Q',
+	'W',
+	'E',
+	'R',
+	'T',
+	'Y',
+	'U',
+	'I',
+	'O',
+	'P',
+	'A',
+	'S',
+	'D',
+	'F',
+	'G',
+	'Z',
+	'X',
+	'C',
+	'V',
+	'B',
+
+	VK_MENU,
+	VK_CONTROL,
+	VK_LSHIFT,
+	VK_SPACE,
+	VK_RETURN,
+	VK_ESCAPE,
+
+	//LAST,
+};
 
 CKeyMgr::CKeyMgr()
 {
@@ -13,4 +52,79 @@ CKeyMgr::~CKeyMgr()
 
 void CKeyMgr::init()
 {
+	for (int i = 0; i < (int)KEY::LAST; i++)
+	{
+		m_vecKey.push_back(tKeyInfo{ KEY_STATE::NONE, false });
+	}
+
+}
+
+void CKeyMgr::update()
+{
+	// 윈도우 포커싱 알아내기
+	//HWND hMainWnd = CCore::GetInst()->GetMainHwnd();
+	HWND hWnd = GetFocus();
+
+	// 윈도우 포커싱 중일 때 키 동작
+	if (nullptr != hWnd) 
+	{
+		for (int i = 0; i < (int)KEY::LAST; i++)
+		{
+			if (GetAsyncKeyState(g_arrVK[i]) & 0x8000)
+			{
+				if (m_vecKey[i].bPrevPush)
+				{
+					// 이전에도 눌려있었다.
+					m_vecKey[i].eState = KEY_STATE::HOLD;
+				}
+				else
+				{
+					// 이전에 눌려있지 않았다.
+					m_vecKey[i].eState = KEY_STATE::TAP;
+				}
+
+				m_vecKey[i].bPrevPush = true;
+			}
+			else
+			{
+				if (m_vecKey[i].bPrevPush)
+				{
+					// 이전에 눌려있었다.
+					m_vecKey[i].eState = KEY_STATE::AWAY;
+					m_vecKey[i].bPrevPush = false;
+				}
+				else
+				{
+					// 이전에도 안눌려있었다.
+					m_vecKey[i].eState = KEY_STATE::NONE;
+				}
+			}
+
+		}
+	}
+	// 윈도우 포커싱 중이 아닐때 키 동작
+	else
+	{
+		for (int i = 0; i < (int)KEY::LAST; i++)
+		{
+			m_vecKey[i].bPrevPush = false;
+
+			if (KEY_STATE::TAP == m_vecKey[i].eState || KEY_STATE::HOLD == m_vecKey[i].eState)
+			{
+				m_vecKey[i].eState = KEY_STATE::AWAY;
+			}
+			else if (KEY_STATE::AWAY == m_vecKey[i].eState)
+			{
+				m_vecKey[i].eState = KEY_STATE::NONE;
+			}
+		}
+
+	}
+
+	
+}
+
+KEY_STATE CKeyMgr::GetKeyState(KEY _eKey)
+{
+	return m_vecKey[(int)_eKey].eState;
 }
--- Client/CKeyMgr.h
+++ Client/CKeyMgr.h
@@ -1,9 +1,54 @@
 #pragma once
 
+// 1. 프레임 동기화
+// 동일 프레임 내에서 같은 키에 대해, 동일한 이벤트를 가져간다.
+
+// 2. 키 입력 이벤트 처리
+// tap, hold, away
+
+enum class KEY_STATE
+{
+	NONE,	// 어떤 상태도 아닌
+	TAP,	// 막 누른 시점
+	HOLD,	// 누르고 있는
+	AWAY,	// 막 뗀 시점
+};
+
+enum class KEY
+{
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN,
+	
+	Q,W,E,R,T,Y,U,I,O,P,
+	A,S,D,F,G,Z,X,C,V,B,
+	
+	ALT,
+	CTRL,
+	LSHIFT,
+	SPACE,
+	ENTER,
+	ESC,
+
+	LAST,
+};
+
+struct tKeyInfo
+{
+	KEY_STATE	eState;	// 키의 상태값
+	bool		bPrevPush;	// 이전 프레임에서 눌렸는지 여부
+};
+
 class CKeyMgr
 {
 	SINGLE(CKeyMgr)
+private:
+	vector<tKeyInfo> m_vecKey;
+
 public:
 	void init();
-private:
+	void update();
+
+	KEY_STATE GetKeyState(KEY _eKey);
 };
