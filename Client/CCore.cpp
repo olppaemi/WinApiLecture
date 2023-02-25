afee39c559186bf9b63f09975e6025928010bf5a
Seung Chan <73764308+olppaemi@users.noreply.github.com>
2023-02-25 09:41:03 +0900

    Double Buffering

--- Client/CCore.cpp
+++ Client/CCore.cpp
@@ -11,6 +11,8 @@ CCore::CCore()
 	: m_hWnd(0)
 	, m_ptResolution{}
 	, m_hDC(0)
+	, m_hBit(0)
+	, m_memDC(0)
 {
 }
 
@@ -18,6 +20,9 @@ CCore::CCore()
 CCore::~CCore()
 {
 	ReleaseDC(m_hWnd, m_hDC);
+
+	DeleteDC(m_memDC);
+	DeleteObject(m_hBit);
 }
 
 int CCore::init(HWND _hWnd, POINT _ptResolution)
@@ -31,6 +36,13 @@ int CCore::init(HWND _hWnd, POINT _ptResolution)
 	SetWindowPos(m_hWnd, nullptr, 100, 100, rt.right - rt.left, rt.bottom - rt.top, 0);
 
 	m_hDC = GetDC(m_hWnd);
+
+	// 이중 버퍼링 용도의 비트맵과 DC를 만든다.
+	m_hBit = CreateCompatibleBitmap(m_hDC, m_ptResolution.x, m_ptResolution.y);
+	m_memDC = CreateCompatibleDC(m_hDC);
+
+	HBITMAP hOldBit = (HBITMAP)SelectObject(m_memDC, m_hBit);
+	DeleteObject(hOldBit);
 
 	// Manager 초기화
 	CTimeMgr::GetInst()->init();
@@ -70,14 +82,21 @@ void CCore::update()
 
 void CCore::render()
 {
+	// 화면 Clear
+	Rectangle(m_memDC, -1, -1, m_ptResolution.x + 1, m_ptResolution.y + 1);
+
+	// 그리기
 	Vec2 vPos = g_obj.GetPos();
 	Vec2 vScale = g_obj.GetScale();
 
 	Rectangle(
-		m_hDC,
+		m_memDC,
 		(int)(vPos.x - vScale.x / 2.f),
 		(int)(vPos.y - vScale.y / 2.f),
 		(int)(vPos.x + vScale.x / 2.f),
 		(int)(vPos.y + vScale.y / 2.f)
 	);
+
+	BitBlt(m_hDC, 0, 0, m_ptResolution.x, m_ptResolution.y
+		, m_memDC, 0, 0, SRCCOPY);
 }
--- Client/CCore.h
+++ Client/CCore.h
@@ -8,6 +8,9 @@ private:
 	POINT	m_ptResolution;
 	HDC		m_hDC;
 
+	HBITMAP m_hBit;
+	HDC m_memDC;
+
 public:
 	int init(HWND _hWnd, POINT _ptResolution);	
 	void progress();
